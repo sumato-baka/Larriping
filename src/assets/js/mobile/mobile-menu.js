2fe27b0f3be6d623841e6464e5c3a2f72501595c
sumato-baka <mail-for-work_0@mail.ru>
2025-01-29 02:13:56 +0300

    Added mobile menu.

---
+++ src/assets/js/mobile/mobile-menu.js
@@ -0,0 +1,25 @@
+let menu_elements = ['mobile-menu__side', 'mobile-menu__base', 'mobile-menu__bottom']
+
+function swapMenuVisibility(show) {
+    menu_elements.forEach(element => {
+        if (show)
+            $(`.${element}`).addClass(`${element}-active`)
+        else
+            $(`.${element}`).removeClass(`${element}-active`)
+    })
+}
+
+$(document).on('scroll', () => {
+    swapMenuVisibility(false);
+})
+
+$('.page-header__mobile-button').on('click', () => {
+    swapMenuVisibility(true);
+})
+
+$(document).on('click', (event) => {
+    if ($(event.target).hasClass('mobile-menu__base'))
+        menu_elements.forEach(element => {
+            $(`.${element}`).removeClass(`${element}-active`)
+        })
+})
