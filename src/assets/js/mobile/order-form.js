38ac594805757d1b68e6288fc4e56dafdc572f51
sumato-baka <none>
2025-02-06 20:27:50 +0300

    '.order-form' improved and device-size adapted.

--- src/assets/js/mobile/order-form.js
+++ src/assets/js/mobile/order-form.js
@@ -1,26 +1,24 @@
-$(window).on('load', () => {
-    $('input').on('input', (event) => {
-        object = event.target;
+$('input').on('input', (event) => {
+    object = event.target;
 
-        if ($(object).hasClass('order-form__phone')) {
-            let regexp = new RegExp($(object).attr('pattern'))
-            let value = $(object).val()
+    if ($(object).hasClass('order-form__phone')) {
+        let regexp = new RegExp($(object).attr('pattern'))
+        let value = $(object).val()
 
-            if (regexp.test(value))
-                object.setCustomValidity('')
-            else
-                object.setCustomValidity('Указан неверный номер телефона.')
-        }
-        else if ($(object).hasClass('order-form__name')) {
-            let regexp = new RegExp($(object).attr('pattern'))
-            let value = $(object).val()
+        if (regexp.test(value))
+            object.setCustomValidity('')
+        else
+            object.setCustomValidity('Указан неверный номер телефона.')
+    }
+    else if ($(object).hasClass('order-form__name')) {
+        let regexp = new RegExp($(object).attr('pattern'))
+        let value = $(object).val()
 
-            if (regexp.test(value))
-                object.setCustomValidity('')
-            else
-                object.setCustomValidity('С заглавной, русскими буквами.')
-        }
-    })
+        if (regexp.test(value))
+            object.setCustomValidity('')
+        else
+            object.setCustomValidity('С заглавной, русскими буквами.')
+    }
 })
 
 $('.order-form__blank').on('submit', (event) => {
