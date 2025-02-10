3c1e9222ca8d06502662ed4bb1d54c8e2c487e4b
sumato-baka <none>
2025-02-10 16:40:09 +0300

    '.image-comparer' refactored- slider is interactable only when user click/touch it instead of click/touch on any of '.image-comparer' inner space.

--- src/assets/js/common/image-comparer.js
+++ src/assets/js/common/image-comparer.js
@@ -1,7 +1,61 @@
-const slider = $('.image-comparer__slider')
+var imageComparerSelector = '.image-comparer'
 
-slider.on('touch').on('input', (e) => {
-    let container = $(e.target).parent('.image-comparer')
+$(document).on('touchstart.image-comparer', event => {
+    if (!$(event.target).hasClass('image-comparer__slider-button'))
+        return
 
-    container.css('--position', `${e.target.value}%`)
-})
+    let comparer = $(event.target).parent(imageComparerSelector)
+    let comparerWidth = comparer.width()
+    let comparerOffsetLeft = comparer.offset().left
+
+    let comparerLeftBound = comparerOffsetLeft
+    let comparerRightBound = comparerOffsetLeft + comparerWidth
+
+    let x
+    let relativeXPercent
+
+    $(document).on('touchmove.image-comparer', event => {
+        x = event.originalEvent.touches[0].clientX
+        relativeXPercent = (x - comparerLeftBound) / comparerWidth * 100
+        console.log(x)
+
+        if (x > comparerLeftBound && x < comparerRightBound)
+            comparer.css('--position', `${relativeXPercent}%`)
+    });
+
+    $(document).on('touchend.image-comparer', () => {
+        $(document).off('touchmove.image-comparer')
+        $(document).off('touchend.image-comparer')
+    });
+});
+
+$(document).on('mousedown.image-comparer', event => {
+    if (event.button != 0)
+        return
+
+    if (!$(event.target).hasClass('image-comparer__slider-button'))
+        return
+
+    let comparer = $(event.target).parent(imageComparerSelector)
+    let comparerWidth = comparer.width()
+    let comparerOffsetLeft = comparer.offset().left
+
+    let comparerLeftBound = comparerOffsetLeft
+    let comparerRightBound = comparerOffsetLeft + comparerWidth
+
+    let x
+    let relativeXPercent
+
+    $(document).on('mousemove.image-comparer', event => {
+        x = event.clientX
+        relativeXPercent = (x - comparerLeftBound) / comparerWidth * 100
+
+        if (x > comparerLeftBound && x < comparerRightBound)
+            comparer.css('--position', `${relativeXPercent}%`)
+    });
+
+    $(document).on('mouseup.image-comparer', () => {
+        $(document).off('mousemove.image-comparer')
+        $(document).off('mouseup.image-comparer')
+    });
+});
